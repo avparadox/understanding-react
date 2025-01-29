fb5e35cf1bc7a169024e3acddfc32e1829ac3467
Sargam Poudel <sargampoudel100@gmail.com>
2025-01-30 00:42:43 +0545

    feat: properly structured parsing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <section>
+      <div>hello World</div>
+      <h1>hello World 2</h1>
+      <div>
+        <h1>hello World 3</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
--- src/babel.ts
+++ src/babel.ts
@@ -4,11 +4,13 @@ import _traverse, { NodePath } from "@babel/traverse";
 import * as t from "@babel/types";
 import * as fs from "fs";
 import path from "path";
+import { displayChild } from "./helper";
 
+console.clear();
 // @ts-expect-error
 const traverse = _traverse.default;
 
-const code = fs.readFileSync(path.join(process.cwd(), "App.tsx"), "utf-8");
+const code = fs.readFileSync(path.join(process.cwd(), "src/App.tsx"), "utf-8");
 console.log("React code:");
 console.log(code);
 
@@ -24,17 +26,6 @@ traverse(ast, {
     }
     const node = path.node;
 
-    const childName = (node.openingElement.name as t.JSXIdentifier).name;
-    console.log(
-      `Found ${childName} element:`,
-      JSON.stringify(
-        {
-          attributes: node.openingElement.attributes,
-          children: node.children,
-        },
-        null,
-        2
-      )
-    );
+    displayChild(node);
   },
 });
